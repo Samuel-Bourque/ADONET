66936fb65b850f01bceac5b02dbb106f22edec31
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 00:55:57 -0500

    Add files via upload

---
+++ labo7-3q5-Samuel-Bourque/ADONET_Services/PCours.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET_Services
+{
+    public class PCours
+    {
+        private static PCours _pCours = null;
+        private string chaineConnexion = "Data Source = info88.cegepthetford.ca; Initial Catalog = C3Q5sambour; Persist Security Info=True;User ID = sambour3q5; Password=Ferland123";
+
+        private PCours()
+        {
+            // Constructeur
+        }
+
+        public static PCours getInstance()
+        {
+            if (_pCours == null)
+                _pCours = new PCours();
+            return _pCours;
+        }
+
+        public void listerCoursSession(int noSession)
+        {
+            using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+            {
+
+                SqlCommand cmdSelectCoursSession = new SqlCommand("Select NOCOURS, TITRE from COURSINFO Where noSession = @noSession", connexion);
+                cmdSelectCoursSession.Parameters.Add("@noSession", SqlDbType.Int);
+                cmdSelectCoursSession.Parameters["@noSession"].Value = noSession;
+                connexion.Open();
+                SqlDataReader reader = cmdSelectCoursSession.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Debug.WriteLine(String.Format("{0}, {1}", reader[0], reader[1]));
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        public int denombrerCoursInfo()
+        {
+            int nbCoursInfo = 0;
+            using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+            {
+                SqlCommand cmdNbCoursInfo = new SqlCommand("SELECT COUNT(*) as nbCours FROM COURSINFO Where NOCOURS like '420%' ", connexion);
+                connexion.Open();
+                nbCoursInfo = (int)cmdNbCoursInfo.ExecuteScalar();
+                connexion.Close();
+            }
+            return nbCoursInfo;
+        }
+
+        public void insererNouveauCours(string noCours, string titre, int theo, int labo, int perso, int noSession)
+        {
+            try
+            {
+                using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+                {
+                    SqlDataAdapter ajoutCours = new SqlDataAdapter("SELECT * FROM COURSINFO", connexion);
+                    SqlCommandBuilder builder = new SqlCommandBuilder(ajoutCours);
+                    DataTable dtCours = new DataTable();
+                    ajoutCours.Fill(dtCours);
+                    DataRow nouvelleRangee = dtCours.NewRow();
+                    nouvelleRangee["NOCOURS"] = noCours;
+                    nouvelleRangee["TITRE"] = titre;
+                    nouvelleRangee["PONDTHEO"] = theo;
+                    nouvelleRangee["PONDLAB"] = labo;
+                    nouvelleRangee["PONDTP"] = perso;
+                    nouvelleRangee["NOSESSION"] = noSession;
+                    dtCours.Rows.Add(nouvelleRangee);
+                    ajoutCours.Update(dtCours);
+                    Debug.WriteLine("Ajout du nouveau cours");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new PCoursException(e.Message);
+            }
+        }
+
+        public void doublerTheorieSession5()
+        {
+            using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+            {
+                SqlCommand cmdDoublerTheo = new SqlCommand("Update COURSINFO Set PONDTHEO = PONDTHEO*2 Where noSession = 5", connexion);
+                connexion.Open();
+                cmdDoublerTheo.ExecuteNonQuery();
+                connexion.Close();
+            }
+        }
+        public int supprimerCoursPhilo()
+        {
+            using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+            {
+                SqlCommand cmdSupprimerCoursPhilo = new SqlCommand("DELETE FROM COURSINFO Where NOCOURS like '340%'", connexion);
+                connexion.Open();
+                int nbCoursSupprimes = (int)cmdSupprimerCoursPhilo.ExecuteNonQuery();
+                connexion.Close();
+                return nbCoursSupprimes;
+            }
+
+        }
+
+        public void listerCoursSessionDataAdapter(int noSession)
+        {
+            using (SqlConnection connexion = new SqlConnection(chaineConnexion))
+            {
+                SqlDataAdapter daCours = new SqlDataAdapter("SELECT NOCOURS, TITRE from COURSINFO Where noSession =" + noSession, connexion);
+                DataSet dsCours = new DataSet();
+                daCours.Fill(dsCours, "COURSINFO");
+                foreach (DataRow tupleCours in dsCours.Tables["COURSINFO"].Rows)
+                {
+                    Debug.WriteLine(String.Format("noCours: {0}", tupleCours["NOCOURS"]), String.Format("titre: {0}", tupleCours["TITRE"]));
+
+                }
+            }
+        }
+    }
+}
---
+++ labo7-3q5-Samuel-Bourque/ADONET_UI/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using ADONET_Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADONET_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AfficherCoursSession(object sender, RoutedEventArgs e)
+        {
+            PCours.getInstance().listerCoursSession(5);
+            tbMessages.AppendText("Operation terminee.\n");
+        }
+
+        private void DeterminerNbCoursInfo(object sender, RoutedEventArgs e)
+        {
+            int nb = PCours.getInstance().denombrerCoursInfo();
+            tbMessages.AppendText($"Nb cours d'info: {nb}\n");
+        }
+
+        private void AjouterCoursTrump(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PCours.getInstance().insererNouveauCours("330-666-RA", "Le monde selon Trump", 3, 3, 3, 6);
+                tbMessages.AppendText("Cours sur Trump ajoute.\n");
+            } catch(PCoursException)
+            {
+                tbMessages.AppendText("Ce cours existe deja.\n");
+            }
+        }
+
+        private void DoublerTheorieSession5(object sender, RoutedEventArgs e)
+        {
+            PCours.getInstance().doublerTheorieSession5();
+            tbMessages.AppendText("Les cours de 5e session ont ete doubles.\n");
+        }
+
+        private void SupprimerCoursPhilo(object sender, RoutedEventArgs e)
+        {
+            int nbCoursSupprimes = 0;
+            nbCoursSupprimes = PCours.getInstance().supprimerCoursPhilo();
+            tbMessages.AppendText("Plus de cours de philo.\n");
+            tbMessages.AppendText("Nombre de cours supprimés : " + nbCoursSupprimes.ToString());
+        }
+
+        private void AfficherCoursSessionDA(object sender, RoutedEventArgs e)
+        {
+            PCours.getInstance().listerCoursSessionDataAdapter(5);
+            tbMessages.AppendText("Operation terminee.\n");
+        }
+    }
+}
